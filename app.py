fefe45a2895d179d805a3c5e0ebe02aacbcc842a
Mayur Chougule <mayurchougule@gmail.com>
2021-09-28 08:57:29 -0400

    restrictions

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/hello')  # this route is not working
 @app.route('/hello/')
 def hello_world():
-    return 'Hello Worlds fixxingbranch after push 928!\n'
+    return 'Hello Worlds checking restriction after push 928!\n'
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
